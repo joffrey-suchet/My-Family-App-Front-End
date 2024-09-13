ff4eb738bc80c489dd6eaefaa53920630d775f8e
joffrey-suchet <joffrey60s@gmail.com>
2024-09-13 18:26:47 +0200

    add button for distribution tasks

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,33 @@
-// import { Button } from "antd";
+import { Button } from "antd";
+import Cookies from "js-cookie";
+import { message } from "antd";
+import axios from "axios";
 
 const Home = () => {
+  const userConnected = JSON.parse(Cookies.get("user"));
+
+  const onClick = async () => {
+    try {
+      const body = { squad: userConnected.squad };
+      const response = await axios.post(
+        "http://localhost:3006/tasks/distribution",
+        body
+      );
+      console.log(response);
+    } catch (error) {
+      console.log("catch", error);
+
+      message.error(error.response.data.message);
+    }
+  };
   return (
     <div className="home_container">
-      <h2>Home</h2>
+      <div className="header">
+        <h2>Home</h2>
+      </div>
+      <Button type="primary" size="small" onClick={onClick}>
+        Répartir les tâches
+      </Button>
     </div>
   );
 };
