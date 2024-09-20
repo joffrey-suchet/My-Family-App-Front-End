f610fdce93d15fbe416b069906b5d5e6722f068d
joffrey-suchet <joffrey60s@gmail.com>
2024-09-20 20:39:04 +0200

    🚧 display users tasks

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,13 @@
-import { Button } from "antd";
+import { Button, Card, List, Tabs } from "antd";
 import Cookies from "js-cookie";
 import { message } from "antd";
 import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
 
 const Home = () => {
   const userConnected = JSON.parse(Cookies.get("user"));
+  const isAdmins = userConnected.role === "admins";
+  const [data, setData] = useState([]);
 
   const onClick = async () => {
     try {
@@ -20,6 +23,45 @@ const Home = () => {
       message.error(error.response.data.message);
     }
   };
+
+  const getData = useCallback(async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3006/users/${userConnected.squad}`
+      );
+      console.log(response.data);
+
+      setData(response.data);
+    } catch (error) {
+      console.log(error);
+      message.error(error.message.data);
+    }
+  }, [userConnected.squad]);
+
+  useEffect(() => {
+    getData();
+  }, [getData]);
+
+  const daysOfWeek = [
+    { value: "monday", label: "Lundi" },
+    { value: "tuesday", label: "Mardi" },
+    { value: "wednesday", label: "Mercredi" },
+    { value: "thursday", label: "Jeudi" },
+    { value: "friday", label: "Vendredi" },
+    { value: "saturday", label: "Samedi" },
+    { value: "sunday", label: "Dimanche" },
+  ];
+
+  const displayUserTasks = (userTasks) =>
+    daysOfWeek.map((day) => (
+      <Card key={day.value} title={day.label}>
+        <List
+          dataSource={userTasks[day.value].reference}
+          renderItem={(item) => <List.Item>{item} </List.Item>}
+        />
+      </Card>
+    ));
+
   return (
     <div className="home_container">
       <div className="header">
@@ -28,6 +70,19 @@ const Home = () => {
       <Button type="primary" size="small" onClick={onClick}>
         Répartir les tâches
       </Button>
+      {isAdmins ? (
+        <Tabs
+          centered
+          defaultActiveKey="1"
+          items={data.map((user, key) => ({
+            key: key,
+            label: user.name,
+            children: displayUserTasks(user.weeklyTasks),
+          }))}
+        />
+      ) : (
+        <p>En construction</p>
+      )}
     </div>
   );
 };
