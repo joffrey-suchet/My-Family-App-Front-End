c3b00093a3a9f4341d490cd0cc1e8d74ee0c6c7c
joffrey-suchet <joffrey60s@gmail.com>
2024-09-27 20:52:24 +0200

    create utils frequency

---
+++ src/utils/frequency.jsx
@@ -0,0 +1,9 @@
+export const frequency = {
+  monday: "Lundi",
+  tuesday: "Mardi",
+  wednesday: "Mercredi",
+  thursday: "Jeudi",
+  friday: "Vendredi",
+  saturday: "Samedi",
+  sunday: "Dimanche",
+};
